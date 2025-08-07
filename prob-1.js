31f3c9c00fbffee53701b128d8af0e217a2faa34
Nayan <n007kingkong@gmail.com>
2025-08-08 01:35:18 +0600

    problem 1 is soved

---
+++ prob-1.js
@@ -0,0 +1,10 @@
+// Take four parameters. Multiply the four numbers and then return the result
+
+
+function multiplication(num1, num2, num3, num4){
+    const answer = num1 * num2 * num3 * num4;
+    return answer;
+}
+
+const answer = multiplication(5, 6, 8, 12);
+console.log(answer);
